26d91b959b0b9d88226e2caa914248a2b7816170
Alex Radu <cooldudev.super@gmail.com>
2025-02-10 17:23:22 -0500

    Add Ball and Camera classes; implement basic game structure and input handling

---
+++ GameDev/Ball.java
@@ -0,0 +1,13 @@
+package GameDev;
+
+public class Ball {
+    private double x, y;
+    private int w, h;
+
+    public Ball(int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+}
---
+++ GameDev/BallTester.java
@@ -0,0 +1,9 @@
+package GameDev;
+
+public class BallTester {
+    public static void main(String[] args) {
+        int x = 4;
+        Ball b1 = new Ball(x, 5, 6, 7);
+        System.out.println(b1.getX());
+    }
+}
